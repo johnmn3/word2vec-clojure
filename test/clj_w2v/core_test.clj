b7588a2bbef96b2b3f32b9a136e2afd3c760017a
tamamu <tamamu.1r1s@gmail.com>
2016-06-23 22:03:41 +0900

    add test

--- test/clj_w2v/core_test.clj
+++ test/clj_w2v/core_test.clj
@@ -2,8 +2,14 @@
   (:require [clojure.test :refer :all]
             [clj-w2v.core :refer :all]))
 
+(deftest file-exists?-test
+  (testing "Function file-exists?")
+    (is (= (file-exists? "dataset.zip")
+           false)))
+
 (deftest count-words-test
   (testing "Function count-words"
     (is (= (sort-word-counts
              (count-words ["a" "b" "c" "d" "d" "e"]))
            '(["d" 2] ["a" 1] ["b" 1] ["c" 1] ["e" 1])))))
+
